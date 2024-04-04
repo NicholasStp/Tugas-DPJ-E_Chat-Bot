c39d801495be7579888e90afbcb1e89e9b00fb11
NicholasSitepu <129504859+NicholasStp@users.noreply.github.com>
2024-04-04 23:39:40 +0700

    Add files via upload

---
+++ long_responses.py
@@ -0,0 +1,12 @@
+import random
+
+R_EATING = "Saya tidak suka makan apa pun karena saya adalah program komputer!"
+R_ADVICE = "Jika saya jadi Anda, saya akan membuka internet dan mengetik persis apa yang Anda tulis di sana!"
+
+
+def unknown():
+    response = ["Bisakah Anda ulangi kalimatnya? ",
+                "...",
+                "Apa maksudnya?"][
+        random.randrange(4)]
+    return response
